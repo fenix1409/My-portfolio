2e1bd0305fed28a6a28d49e8dc35d68ac0eb3397
fenix1409 <b2550486@gmail.com>
2025-03-20 10:31:29 +0500

    done

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,9 +3,9 @@ export const contactsData = {
     phone: '+998977772439',
     address: 'Yunusabad, Tashkent, Uzbekistan',
     github: 'https://github.com/fenix1409',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
+    facebook: 'https://www.facebook.com',
+    linkedIn: 'https://www.linkedin.com/in/boburjon-oripov-02064a315',
+    twitter: 'https://twitter.com',
     stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
     devUsername: "Bobur Oripov"
 }
