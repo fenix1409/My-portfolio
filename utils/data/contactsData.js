e85ec7533ed96a9c5455cb725306545023af85c1
fenix1409 <b2550486@gmail.com>
2025-03-20 16:38:00 +0500

    add

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -4,7 +4,7 @@ export const contactsData = {
     address: 'Yunusabad, Tashkent, Uzbekistan',
     github: 'https://github.com/fenix1409',
     facebook: 'https://www.facebook.com',
-    linkedIn: 'https://www.linkedin.com/in/boburjon-oripov-02064a315',
+    linkedIn: 'https://www.linkedin.com/in/bobur-oripov-02064a315/',
     twitter: 'https://twitter.com',
     stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
     devUsername: "Bobur Oripov"
