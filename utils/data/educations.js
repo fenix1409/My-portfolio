821bd6f6e714c6853f2eaf99506ab1c7606951e4
fenix1409 <b2550486@gmail.com>
2025-06-01 15:53:57 +0500

    done

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -4,11 +4,5 @@ export const educations = [
     title: "NT Certificate in Frontend Development",
     duration: "April 2024 - January 2025",
     institution: "Najot Ta'lim",
-  },
-  {
-    id: 2,
-    title: "Higher Secondary",
-    duration: "2014 - 2025",
-    institution: "School 240",
   }
 ]
